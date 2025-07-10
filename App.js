d58ba6f4fc6fc21517d218093c4c5fd348169f08
Truongle68 <truonglephuc11@gmail.com>
2025-07-10 16:05:08 +0700

    Build CartContext to manage cart state

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigations/RootNavigation";
 import { AuthProvider } from "./src/contexts/AuthContext";
 import Toast from "react-native-toast-message";
+import { CartProvider } from "./src/contexts/CartContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-      <Toast />
+      <CartProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+        <Toast />
+      </CartProvider>
     </AuthProvider>
   );
 }
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,51 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "../utils/axiosPrivate";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const fetchCart = async () => {
+    try {
+      const res = await axios.get("/carts");
+      console.log("cart: ", res);
+      setCart(res.data);
+    } catch (err) {
+      console.error("Lỗi fetch cart:", err);
+    }
+  };
+
+  const addToCart = async (item) => {
+    await axios.post("/carts", item);
+    await fetchCart();
+  };
+
+  const updateProductQuantityInCart = async (id, Quantity) => {
+    await axios.patch(`/carts/${id}`, { Quantity });
+    await fetchCart();
+  };
+
+  const removeFromCart = async (id) => {
+    await axios.delete(`/carts/${id}`);
+    await fetchCart();
+  };
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        fetchCart,
+        addToCart,
+        updateProductQuantityInCart,
+        removeFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
